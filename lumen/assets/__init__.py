c8a169ce60069f90712e0c733acee2bd153cc6f8
Colin Son <colinson@fastmail.com>
2026-07-02 11:03:11 -0500

    Add segmented anatomy import for fluoro roadmaps
    Signed-off-by: Colin Son <colinson@fastmail.com>

--- lumen/assets/__init__.py
+++ lumen/assets/__init__.py
@@ -1,3 +1,7 @@
 from lumen.assets.schema import Asset, DeviceSpawn, Edge, Frame, Node
+from lumen.assets.imaging import (Volume, asset_from_mask, load_dicom_series, load_npz_volume,
+                                  segment_threshold)
 
-__all__ = ["Asset", "DeviceSpawn", "Edge", "Frame", "Node"]
+__all__ = ["Asset", "DeviceSpawn", "Edge", "Frame", "Node",
+           "Volume", "load_npz_volume", "load_dicom_series", "segment_threshold",
+           "asset_from_mask"]
